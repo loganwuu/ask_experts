a53f26c8ebae23f7f7ed3ab1d6f73f5f6a0df459
logan wu & chris swan <student@epicodus.com>
2015-09-23 16:54:55 -0700

    Start adding comments to the answers.

--- controllers/AnsweredController.js
+++
@@ -1,3 +0,0 @@
-AskExperts.controller('AnsweredCtrl', function AnsweredCtrl($scope) {
-  $scope.addAnswer
-})
--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -1,4 +1,4 @@
-AskExperts.controller('CommentsCtrl', function CommentsCtrl($scope, $stateParams, AnswersFactory, UtilitiesFactory) {
+AskExperts.controller('CommentsCtrl', function CommentsCtrl($scope, $stateParams, AnswersFactory, QuestionsFactory, UtilitiesFactory) {
   $scope.answer = UtilitiesFactory.findById(AnswersFactory.answers, $stateParams.answerId);
   $scope.addComment = function() {
     $scope.answer.comments.push({ name: $scope.commentName });
