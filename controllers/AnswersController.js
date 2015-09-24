8a3e1f3b4fe15c9de4a2df94d30f4ab03b3a2dbf
chris swan & logan wu <student@epicodus.com>
2015-09-24 10:22:28 -0700

    add feature to rate answers and organize based on rating.

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -1,8 +1,20 @@
 AskExperts.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, QuestionsFactory, UtilitiesFactory) {
   $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId);
   $scope.addAnswer = function() {
-    $scope.question.answers.push({ name: $scope.answerName });
+    $scope.question.answers.push({ name: $scope.answerName, comments: [], upvotes: 0, downvotes: 0, totalVote: 0 });
     $scope.question.answered = true;
     $scope.answerName = null;
-  }
+  };
+
+  $scope.addUpvote = function(answer) {
+    var index = $scope.question.answers.indexOf(answer);
+    $scope.question.answers[index].upvotes += 1;
+    $scope.question.answers[index].totalVote += 1;
+  };
+
+  $scope.addDownvote = function(answer) {
+    var index = $scope.question.answers.indexOf(answer);
+    $scope.question.answers[index].downvotes -= 1;
+    $scope.question.answers[index].totalVote -= 1;
+  };
 });
